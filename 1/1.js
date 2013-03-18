1662ebaa07b9accba8204d599ae6d4badc15b2d2
vagrant <vagrant@precise64.(none)>
2013-03-18 21:50:42 +0000

    Adding Example 1

---
+++ 1/1.js
@@ -0,0 +1,9 @@
+/* Hands On 1 - Hello World, Simply JS @ Server */
+console.log('hello world');
+
+var a = ['superman', 'batman'];
+a.push('bülent');
+console.log(a);
+
+var employee = {name: 'timo juers', department: 'IT/AQM' };
+console.log(employee); 
